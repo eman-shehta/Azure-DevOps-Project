5ad94bd1a6d4ad2ce2ce4d993c4dcb7df534ba98
eman shehta <emanshshta@gmail.com>
2025-05-11 00:57:15 +0300

    test

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ def test_get_messages_returns_200():
 def test_post_message_returns_201():
     with app.test_client() as client:
         response = client.post('/api/message', json={'message': 'Test from pytest'})
-        assert response.status_code in [201, 500]  # 500 if DB not ready
+        assert response.status_code in [201, 500]  # 500 if DB not ready 
